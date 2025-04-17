0958dff4863af6cf0ecd9d3e80ca24114e53c8f5
Sudharaka Pathiraja <114396209+Sudharaka200@users.noreply.github.com>
2025-04-17 13:00:53 +0530

    order place done

--- pages/orderSummary.php
+++ pages/orderSummary.php
@@ -13,9 +13,16 @@
     <!-- import files -->
     <?php include '../library/head.php'; ?>
     <?php include '../library/nav.php'; ?>
+    <?php include '../library/db_conn.php'; ?>
 
     <!-- Order summary -->
     <section class="py-24 relative">
+    <?php 
+        $N_product_id = $_REQUEST["N_product_id"];
+        $sql = "SELECT * FROM new_arrival_products WHERE N_product_id='$N_product_id' ";
+        $result = $conn->query($sql);
+        while($row = $result->fetch_assoc()) {
+    ?>
         <div class="w-full max-w-7xl px-4 md:px-5 lg-6 mx-auto">
             <h2 class="font-manrope font-bold text-4xl leading-10 text-black text-center">
                 Payment Successful
@@ -27,7 +34,7 @@
                 <div
                     class="flex flex-col lg:flex-row lg:items-center justify-between px-6 pb-6 border-b border-gray-200">
                     <div class="data">
-                        <p class="font-semibold text-base leading-7 text-black">Order Id: <span class="text-indigo-600 font-medium">#10234987</span></p>
+                        <p class="font-semibold text-base leading-7 text-black">Order Id: <span class="text-indigo-600 font-medium">#1023<?php echo $row["N_product_id"] ?></span></p>
                         <p class="font-semibold text-base leading-7 text-black mt-4">Order Payment : <span class="text-gray-400 font-medium"> 18th march
                                 2021</span></p>
                     </div>
@@ -37,7 +44,7 @@
                 <div class="w-full px-3 min-[400px]:px-6">
                     <div class="flex flex-col lg:flex-row items-center py-6 border-b border-gray-200 gap-6 w-full">
                         <div class="img-box max-lg:w-full">
-                            <img src="https://pagedone.io/asset/uploads/1701167607.png" alt="Premium Watch image"
+                            <img src=<?php echo $row["N_product_img1"] ?> alt="Premium Watch image"
                                 class="aspect-square w-full lg:max-w-[140px] rounded-xl object-cover">
                         </div>
                         <div class="flex flex-row items-center w-full ">
@@ -45,15 +52,10 @@
                                 <div class="flex items-center">
                                     <div class="">
                                         <h2 class="font-semibold text-xl leading-8 text-black mb-3">
-                                            Premium Quality Dust Watch</h2>
+                                        <?php echo $row["N_product_name"] ?></h2>
                                         <p class="font-normal text-lg leading-8 text-gray-500 mb-3 ">
                                             By: Dust Studios</p>
                                         <div class="flex items-center ">
-                                            <p
-                                                class="font-medium text-base leading-7 text-black pr-4 mr-4 border-r border-gray-200">
-                                                Size: <span class="text-gray-500">100 ml</span></p>
-                                            <p class="font-medium text-base leading-7 text-black ">Qty: <span
-                                                    class="text-gray-500">2</span></p>
                                         </div>
                                     </div>
 
@@ -62,7 +64,7 @@
                                     <div class="col-span-5 lg:col-span-1 flex items-center max-lg:mt-3">
                                         <div class="flex gap-3 lg:block">
                                             <p class="font-medium text-sm leading-7 text-black">price</p>
-                                            <p class="lg:mt-4 font-medium text-sm leading-7 text-indigo-600">$100</p>
+                                            <p class="lg:mt-4 font-medium text-sm leading-7 text-indigo-600">$<?php echo $row["N_product_price"] ?></p>
                                         </div>
                                     </div>
                                     <div class="col-span-5 lg:col-span-2 flex items-center max-lg:mt-3 ">
@@ -72,62 +74,6 @@
                                             <p
                                                 class="font-medium text-sm leading-6 whitespace-nowrap py-0.5 px-3 rounded-full lg:mt-3 bg-emerald-50 text-emerald-600">
                                                 Ready for Delivery</p>
-                                        </div>
-
-                                    </div>
-                                    <div class="col-span-5 lg:col-span-2 flex items-center max-lg:mt-3">
-                                        <div class="flex gap-3 lg:block">
-                                            <p class="font-medium text-sm whitespace-nowrap leading-6 text-black">
-                                                Expected Delivery Time</p>
-                                            <p class="font-medium text-base whitespace-nowrap leading-7 lg:mt-3 text-emerald-500">
-                                                23rd March 2021</p>
-                                        </div>
-
-                                    </div>
-                                </div>
-                            </div>
-
-
-                        </div>
-                    </div>
-
-                    <div class="flex flex-col lg:flex-row items-center py-6 gap-6 w-full">
-                        <div class="img-box max-lg:w-full">
-                            <img src="https://pagedone.io/asset/uploads/1701167621.png" alt="Diamond Watch image"
-                                class="aspect-square w-full lg:max-w-[140px] rounded-xl object-cover">
-                        </div>
-                        <div class="flex flex-row items-center w-full ">
-                            <div class="grid grid-cols-1 lg:grid-cols-2 w-full">
-                                <div class="flex items-center">
-                                    <div class="">
-                                        <h2 class="font-semibold text-xl leading-8 text-black mb-3 ">
-                                            Diamond Platinum Watch</h2>
-                                        <p class="font-normal text-lg leading-8 text-gray-500 mb-3">
-                                            Diamond Dials</p>
-                                        <div class="flex items-center  ">
-                                            <p
-                                                class="font-medium text-base leading-7 text-black pr-4 mr-4 border-r border-gray-200">
-                                                Size: <span class="text-gray-500">Regular</span></p>
-                                            <p class="font-medium text-base leading-7 text-black ">Qty: <span
-                                                    class="text-gray-500">1</span></p>
-                                        </div>
-                                    </div>
-
-                                </div>
-                                <div class="grid grid-cols-5">
-                                    <div class="col-span-5 lg:col-span-1 flex items-center max-lg:mt-3">
-                                        <div class="flex gap-3 lg:block">
-                                            <p class="font-medium text-sm leading-7 text-black">price</p>
-                                            <p class="lg:mt-4 font-medium text-sm leading-7 text-indigo-600">$100</p>
-                                        </div>
-                                    </div>
-                                    <div class="col-span-5 lg:col-span-2 flex items-center max-lg:mt-3 ">
-                                        <div class="flex gap-3 lg:block">
-                                            <p class="font-medium text-sm leading-7 text-black">Status
-                                            </p>
-                                            <p
-                                                class="font-medium text-sm leading-6 py-0.5 px-3 whitespace-nowrap rounded-full lg:mt-3 bg-indigo-50 text-indigo-600">
-                                                Dispatched</p>
                                         </div>
 
                                     </div>
@@ -161,11 +107,12 @@
                         </button>
                         <p class="font-medium text-lg text-gray-900 pl-6 py-3 max-lg:text-center">Paid using Credit Card <span class="text-gray-500">ending with 8822</span></p>
                     </div>
-                    <p class="font-semibold text-lg text-black py-6">Total Price: <span class="text-indigo-600"> $200.00</span></p>
+                    <p class="font-semibold text-lg text-black py-6">Total Price: <span class="text-indigo-600"> $<?php echo $row["N_product_price"] ?></span></p>
                 </div>
 
             </div>
         </div>
+    <?php } ?>
     </section>
 
 </body>
