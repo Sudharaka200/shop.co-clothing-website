7543f9954674d71ca2e05b7c1e3a9cb2c230770b
Sudharaka Pathiraja <114396209+Sudharaka200@users.noreply.github.com>
2025-04-17 15:33:19 +0530

    update

---
+++ pages/sql/order-u.php
@@ -0,0 +1,14 @@
+<?php
+
+
+//DB connn
+include '../../library/db_conn.php';
+
+
+echo $N_product_name = $_REQUEST["N_product_name"];
+echo $N_product_price = $_REQUEST["N_product_price"];
+echo $N_product_img1 = $_REQUEST["N_product_img1"];
+
+
+
+?>
